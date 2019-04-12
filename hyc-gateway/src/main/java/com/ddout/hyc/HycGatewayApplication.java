12526b63e07c565d97805806d64363e3dc07062c
ddout <ddout@qq.com>
2019-04-12 14:13:28 +0800

    update config

--- hyc-gateway/src/main/java/com/ddout/hyc/HycGatewayApplication.java
+++ hyc-gateway/src/main/java/com/ddout/hyc/HycGatewayApplication.java
@@ -2,8 +2,10 @@ package com.ddout.hyc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringCloudApplication
+@EnableEurekaClient
 public class HycGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(HycGatewayApplication.class, args);
